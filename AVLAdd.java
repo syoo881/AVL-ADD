ac9f5de4ee8403368669345a6fc74bd687d13d5f
Seth Yoo <syoo881@aucklanduni.ac.nz>
2023-08-20 21:33:28 +1200

    implemented insert and Traverse method, for BST. Need at add rotations

--- AVLAdd.java
+++ AVLAdd.java
@@ -1,9 +1,41 @@
 public class AVLAdd<T extends Comparable<T>> implements Tree<T>{
 
+
+    private Node<T> root;
     @Override
     public Tree<T> insert(T data) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'insert'");
+        root = insert(data, root);
+        return this;
     }
+
+    private Node<T> insert(T data, Node<T> node) {
+        if (node == null) {
+            return new Node<>(data);
+        }
+        if (data.compareTo(node.getData()) < 0) {
+            node.setLeftChild(insert(data, node.getLeftChild()));
+        } else if (data.compareTo(node.getData()) > 0) {
+            node.setRightChild(insert(data, node.getRightChild()));
+        } else {
+            return node;
+        }
+        return node;
+    }
+
+    @Override
+    public void traverse() {
+        traverseInOrder(root);
+    }
+
+    private void traverseInOrder(Node<T> node) {
+        if (node == null) {
+            return;
+        }
+        traverseInOrder(node.getLeftChild());
+        System.out.println(node.getData());
+        traverseInOrder(node.getRightChild());
+    }
+
+    
     
 }
--- Node.java
+++ Node.java
@@ -1,15 +1,47 @@
 public class Node<T extends Comparable<T>>{
     
-    private int height;
+    private int height =1;
     private T data;
     private Node<T> leftChild;
     private Node<T> rightChild;
 
     public Node(T data){
         this.data = data;
-        this.height = height;
+        this.height = 1;
         this.leftChild = null;
         this.rightChild = null;
+    }
+
+    public int getHeight(){
+        return height;
+    }
+
+    public void setHeight(int height){
+        this.height = height;
+    }
+
+    public T getData(){
+        return data;
+    }
+
+    public void setData(T data){
+        this.data = data;
+    }
+
+    public Node<T> getLeftChild(){
+        return leftChild;
+    }
+
+    public void setLeftChild(Node<T> leftChild){
+        this.leftChild = leftChild;
+    }
+
+    public Node<T> getRightChild(){
+        return rightChild;
+    }
+
+    public void setRightChild(Node<T> rightChild){
+        this.rightChild = rightChild;
     }
 
 }
